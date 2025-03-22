d9547722f7fb1e1f40fffd26524e01f5f515ab32
yoohyunk <yoohyunk20@gmail.com>
2025-03-22 00:24:28 -0600

    set up prettier and format on save

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,5 @@
+import prettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -11,6 +13,10 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: { prettier: eslintPluginPrettier },
+    rules: { ...prettier.rules, "prettier/prettier": "error" },
+  },
 ];
 
 export default eslintConfig;
