3f583ef3d7d59f571e28eafbdc662c8f04cd28fb
yoohyunk <yoohyunk20@gmail.com>
2025-03-22 22:43:57 -0600

    added user sidebar for logged in users and pages for logged in users

---
+++ app/payment/page.tsx
@@ -0,0 +1,7 @@
+export default function paymentPage() {
+  return (
+    <div>
+      <h1>payment</h1>
+    </div>
+  );
+}
---
+++ components/UserSidebar.tsx
@@ -0,0 +1,52 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { auth } from "@/firebase/firebaseConfig";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { Separator } from "@radix-ui/react-separator";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+
+interface UserSidebarProps {
+  onSignOut: () => void;
+}
+
+export default function UserSidebar({ onSignOut }: UserSidebarProps) {
+  const [open, setopen] = useState(false);
+
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setopen(false);
+  }, [pathname]);
+
+  return (
+    <Sheet open={open} onOpenChange={setopen}>
+      <SheetTrigger onClick={() => setopen(true)}>Account</SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Account</SheetTitle>
+          <SheetDescription>
+            Hi,
+            {auth.currentUser?.displayName}
+          </SheetDescription>
+        </SheetHeader>
+        <div className="flex flex-col gap-2 text-center w-full">
+          <Separator className="border-t border-gray-200" />
+          <Link href="/orders">Orders</Link>
+          <Separator className="border-t border-gray-200" />
+          <Link href="/profile">Profile</Link>
+          <Separator className="border-t border-gray-200" />
+          <Link href="/payment">Payment</Link>
+          <Button onClick={onSignOut}>log out</Button>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
