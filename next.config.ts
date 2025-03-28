00bed3d035b22c2b27ee8c21afd9e4639d6f2755
yoohyunk <163964321+yoohyunk@users.noreply.github.com>
2025-03-28 01:33:20 -0600

    Square api items (#21)
    * improve header syling
    
    * add get-catalog api to get items to display on shop page
    
    * move data transform logic to route handler
    
    * clean up comments

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["items-images-sandbox.s3.us-west-2.amazonaws.com"],
+  },
 };
 
 export default nextConfig;
